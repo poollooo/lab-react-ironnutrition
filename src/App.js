68ac997e8a867c64e9be467953474bd2147e50c8
Paul Nizet <polonizet@gmail.com>
2022-09-07 14:54:32 +0200

    render foodlist in rows

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { React, useState } from 'react';
+import foods from './foods.json';
 import './App.css';
+import FoodBox from './components/FoodBox';
+import { Row, Divider, Button } from 'antd';
 
 function App() {
+  const [food, setFood] = useState(foods);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {foods.map((food) => {
+          return (
+            <FoodBox food={{ ...food }} />
+          )
+        })}
+      </Row>
     </div>
   );
 }
